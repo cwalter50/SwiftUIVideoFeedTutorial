ca9057efe7d84e0d1b92f8a5ef99b6f083208ce5
Chris Walter <walt50@gmail.com>
2024-07-27 20:36:36 -0400

    Initial Commit

---
+++ SwiftUIVideoFeedTutorial/SwiftUIVideoFeedTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIVideoFeedTutorialApp.swift
+//  SwiftUIVideoFeedTutorial
+//
+//  Created by Christopher Walter on 7/27/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIVideoFeedTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
